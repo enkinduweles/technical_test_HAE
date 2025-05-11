b54175a533f6e7f81cebf146e1a64573de639bce
enkindu <enkinduweles@gmail.com>
2025-05-10 21:08:38 -0300

    chore: add main structure layout as header, sidebar and movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Movies } from './components/Movies';
+
 function App() {
-  return <div>INITITAL</div>;
+  return (
+    <main className="h-screen bg-[#F5F5F3]">
+      <Header />
+      <div className="w-full flex h-[calc(100vh-55px)] ">
+        <Sidebar />
+        <Movies />
+      </div>
+    </main>
+  );
 }
 
 export default App;
