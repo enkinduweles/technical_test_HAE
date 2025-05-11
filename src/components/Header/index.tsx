28df7093ade5f27d0385c31583c339a8b718878b
enkindu <enkinduweles@gmail.com>
2025-05-11 19:46:31 -0300

    refactor: add 3 lines icon to open menu on mobile devices

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
 import reactLogo from '/react.svg';
+import menu from '/menu.svg';
+import { useDrawerContext } from '../../context/Drawer';
 
 export function Header() {
+  const { onOpenDrawer } = useDrawerContext();
   return (
     <header className="w-full h-[55px] px-4 py-2 border-b-1 border-b-stone-500 bg-[#3B3E42]">
       <div className="flex items-center gap-4">
-        <img src={reactLogo} className="logo" alt="ReactJs logo" />
+        <img
+          src={menu}
+          alt="Menu logo"
+          className="block md:hidden cursor-pointer"
+          onClick={() => onOpenDrawer()}
+        />
+        <img src={reactLogo} alt="ReactJs logo" />
         <h1 className="text-white">Technical Test - List of Movies</h1>
       </div>
     </header>
