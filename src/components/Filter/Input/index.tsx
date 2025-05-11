45ae73189d3a4ce342082be6ea4a9fe3e3464018
enkindu <enkinduweles@gmail.com>
2025-05-11 19:45:57 -0300

    refactor: save filter input in context

--- src/components/Filter/Input/index.tsx
+++ src/components/Filter/Input/index.tsx
@@ -1,15 +1,21 @@
+import { useFilterContext } from '../../../context/FilterContext';
 import { Icon } from '../../Icon';
 import searchIcon from '/search.svg';
 
 export function Input() {
+  const { setInputText, inputText } = useFilterContext();
   return (
     <>
-      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none self">
+      <div className="absolute left-0 flex items-center pl-3 pointer-events-none">
         <Icon icon={searchIcon} altText="Search icon" />
       </div>
       <input
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+        value={inputText}
         type="text"
-        className="pl-10 bg-white border border-gray-600 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+        className="pl-10 bg-white border border-stone-600 text-[#3B3E42] text-sm rounded-lg block w-full p-1"
         placeholder="Search..."
       />
     </>
